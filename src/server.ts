2bf98a4b447b4e8157ac717886c5cf5099b09de6
Reinaldo Alves <reinaldoasjr8@gmail.com>
2023-08-31 17:17:16 -0300

    static files access allowed

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ config();
 const app = express();
 
 const cors = require('cors');
+const path = require('path');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -24,6 +25,9 @@ app.use((req, res, next) => {
 app.use(cors());
 
 app.use(express.json());
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use('/user', userRoutes);
 app.use('/medium', mediumRoutes);
 app.use('/falange', falangeRoutes);
