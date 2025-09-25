e1a07c0c3311f735a47449d8b27d925ce304039a
Reinaldo Alves <reinaldoasjr8@gmail.com>
2025-09-25 14:47:01 -0300

    add production db connection object

--- src/mysql.ts
+++ src/mysql.ts
@@ -3,12 +3,21 @@ import { config } from 'dotenv';
 
 config();
 
-const pool = mysql.createPool({
+const localConnectionObj = {
     "user":process.env.USER_DATABASE,
     "password":process.env.PASSWORD_DATABASE,
     "database":process.env.DATABASE,
     "host":process.env.HOST_DATABASE,
     "port":Number(process.env.PORT_DATABASE)
-})
+}
+
+const productionConnectionObj = {
+    "user":process.env.USER_DATABASE,
+    "password":process.env.PASSWORD_DATABASE,
+    "database":process.env.DATABASE,
+    "socketPath": process.env.HOST_DATABASE
+}
+
+const pool = mysql.createPool(process.env.ENVIRONMENT === 'local' ? localConnectionObj : productionConnectionObj);
 
 export { pool };
